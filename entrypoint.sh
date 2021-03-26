805cd04862bb8947121e246a07e3fed372b382ea
mclk623 <66455829+mclk623@users.noreply.github.com>
2021-03-26 20:31:48 +0800

    更新文件
    更新

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ cd /home/container
 MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
 echo -e ":/home/container$ ${START_COMMAND}"
-
+#设置文件权限
+chmod 775 *
 # Run the Server
 echo 正在启动服务器
 ${MODIFIED_STARTUP}
